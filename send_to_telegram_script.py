0b9db81990a9ab4132c7227c995242937d799bd6
Lemanos <guiobn@gmail.com>
2023-10-22 23:55:20 +0300

    Update send_to_telegram_script.py

--- send_to_telegram_script.py
+++ send_to_telegram_script.py
@@ -2,6 +2,9 @@
 from telegram import Bot
 from telegram import InputFile
 
-def send_to_telegram(pdf_filename, chat_id, bot_token):
+#def send_to_telegram(pdf_filename, chat_id, bot_token):
+ #   bot = Bot(token=bot_token)
+  #  bot.send_document(chat_id=chat_id, document=open(pdf_filename, 'rb'))
+def send_to_telegram(message_text, chat_id, bot_token):
     bot = Bot(token=bot_token)
-    bot.send_document(chat_id=chat_id, document=open(pdf_filename, 'rb'))
+    bot.send_message(chat_id=chat_id, text=message_text)
